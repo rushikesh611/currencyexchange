56412b5023234535d3c18292e1c655603abede59
Hrushikesh Sampat Jadhav <hrushikesh.jadhav@accolite.com>
2023-02-18 10:14:12 +0530

    init

---
+++ src/main/java/com/example/currencyexchange/CurrencyexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyexchangeApplication.class, args);
+	}
+
+}
